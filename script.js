7b5e91a1ffec25e4e14f952263fc7610563c763e
Jonathan Andersson <112062438+Sleepy-Koala@users.noreply.github.com>
2022-11-03 15:33:36 +0100

    Added all

---
+++ script.js
@@ -0,0 +1,7 @@
+let box = document.getElementById('box');
+let changer = document.getElementById('changer');
+
+changer.addEventListener('click', function(){
+	box.classList.toggle('orangeBox');
+});
+
